d4b60d1713945c79ecf8e47b5cdf9c4d66aeb9a3
ngocnguyen3110 <ngoc311097@gmail.com>
2024-04-26 22:17:14 +0700

    Commit demo

---
+++ Demo/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Xin chao thay giao, em la Nguyen Thi Kim Ngoc");
+    }
+}

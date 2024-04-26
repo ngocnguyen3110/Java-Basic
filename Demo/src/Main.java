3725950f6e02216edad8cef2e9e54792567d3cf7
ngocnguyen3110 <168014161+ngocnguyen3110@users.noreply.github.com>
2024-04-26 22:23:29 +0700

    Update Main.java

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Xin chao thay giao, em la Nguyen Thi Kim Ngoc");
         System.out.println("Em den tu Ha Noi");
+        System.out.println("Em nop bai tap Buoi 1 nha thay");
     }
 }
